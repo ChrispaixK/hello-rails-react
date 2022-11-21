4f8143af6ba79ce8c58d42540e794ba0f03cdb24
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-21 19:02:02 +0200

    create rails react app and display data from api endpoint

---
+++ app/javascript/pages/Greeting.js
@@ -0,0 +1,15 @@
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchGreeting} from "../store/greetings";
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greetings.greeting);
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, []);
+
+  return <h3>{greeting}</h3>
+}
+
+export default Greeting
